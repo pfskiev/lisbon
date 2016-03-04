8e1c24b039ce3497120db6ecde8d7c38ce8f463c
konstantin <Rhfrfpz,hf1301>
2016-03-04 14:52:19 +0200

    chages

--- src/tours/models.py
+++ src/tours/models.py
@@ -78,7 +78,6 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-
     def __unicode__(self):
         return self.title
 
