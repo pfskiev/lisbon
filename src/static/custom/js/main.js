78f60962c34d10a0c3c20a31fd8189e8709e1b3e
konstantin <Rhfrfpz,hf1301>
2016-03-17 18:37:14 +0200

    contact

--- src/static/custom/js/main.js
+++ src/static/custom/js/main.js
@@ -1,21 +1,17 @@
 $('document').ready(function() {
-
     var _id = '';
-
-
     $(".datepicker").datepicker();
     $('.phone').mask('0(000)-000-00-00');
     $('.btn.btn-warning').each(function(){
-    $(this).addClass('shadow')
-})
-
-})
+        $(this).addClass('shadow')
+    })
+});
 
 function delete_contact(id) {
-        _id = id
-    }
-    function confirm_delete() {
-        window.location.pathname = '/tours/' + _id + '/delete/'
-    }
+    _id = id
+}
+function confirm_delete() {
+    window.location.pathname = '/tours/' + _id + '/delete/'
+}
 
 
