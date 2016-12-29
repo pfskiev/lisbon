80b662e9586b7ca3dd55f5549da8c5702404989a
pfskiev <podlesny@outlook.com>
2016-12-29 12:05:16 +0200

    delete email settings

--- src/lisbon/settings/mail.py
+++
@@ -1,9 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'danichisce@gmail.com'
-EMAIL_HOST_PASSWORD = 'lisboa2015'
-EMAIL_PORT = 587
-EMAIL_TO = [
-    'reservationgoldenlisbon@gmail.com',
-    'podlesny@outlook.com'
-]
