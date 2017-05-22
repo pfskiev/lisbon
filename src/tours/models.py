a7c91e77376a5836648648bfcb31ea593e7ac3f6
pfskiev <podlesny@outlook.com>
2017-05-22 13:36:48 +0300

    update description field in tour model

--- src/tours/models.py
+++ src/tours/models.py
@@ -8,9 +8,9 @@ class BaseClass(models.Model):
     title_PT = models.CharField(_('Title PT'), max_length=100, blank=True, null=False)
     title_EN = models.CharField(_('Title EN'), max_length=100, blank=True, null=False)
     title_DE = models.CharField(_('Title DE'), max_length=100, blank=True, null=False)
-    description_PT = models.TextField(_('Description PT'), max_length=1000, blank=True, null=False)
-    description_EN = models.TextField(_('Description EN'), max_length=1000, blank=True, null=False)
-    description_DE = models.TextField(_('Description DE'), max_length=1000, blank=True, null=False)
+    description_PT = models.TextField(_('Description PT'), max_length=2000, blank=True, null=False)
+    description_EN = models.TextField(_('Description EN'), max_length=2000, blank=True, null=False)
+    description_DE = models.TextField(_('Description DE'), max_length=2000, blank=True, null=False)
     keywords_SEO = models.TextField(_('Keywords for SEO'), max_length=2000, blank=True, null=False)
     description_SEO = models.TextField(_('Description for SEO'), max_length=2000, blank=True, null=False)
 
