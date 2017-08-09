8b7b3ebac5d5997528ac3d47a65c6deaad09f071
pfskiev <podlesny@outlook.com>
2017-08-09 21:25:42 +0300

    fix

--- src/gallery/models.py
+++ src/gallery/models.py
@@ -7,9 +7,9 @@ class Gallery(models.Model):
     title_PT = models.CharField(_('Title PT'), max_length=100, blank=True, null=False)
     title_EN = models.CharField(_('Title EN'), max_length=100, blank=True, null=False)
     title_DE = models.CharField(_('Title DE'), max_length=100, blank=True, null=False)
-    description_PT = models.TextField(_('Description PT'), max_length=1000, blank=True, null=False)
-    description_EN = models.TextField(_('Description EN'), max_length=1000, blank=True, null=False)
-    description_DE = models.TextField(_('Description DE'), max_length=1000, blank=True, null=False)
+    description_PT = models.TextField(_('Description PT'), max_length=5000, blank=True, null=False)
+    description_EN = models.TextField(_('Description EN'), max_length=5000, blank=True, null=False)
+    description_DE = models.TextField(_('Description DE'), max_length=5000, blank=True, null=False)
     img = models.ImageField(_('Photo # 1'), null=True, blank=True)
     img_1 = models.ImageField(_('Photo # 2'), null=True, blank=True)
     img_2 = models.ImageField(_('Photo # 3'), null=True, blank=True)
@@ -17,8 +17,8 @@ class Gallery(models.Model):
     img_4 = models.ImageField(_('Photo # 5'), null=True, blank=True)
     img_5 = models.ImageField(_('Photo # 5'), null=True, blank=True)
     video = models.URLField(_('Video url'), max_length=100, blank=True, null=False)
-    keywords_SEO = models.TextField(_('Keywords SEO'), max_length=1000, blank=True, null=False)
-    description_SEO = models.TextField(_('Description SEO'), max_length=1000, blank=True, null=False)
+    keywords_SEO = models.TextField(_('Keywords SEO'), max_length=5000, blank=True, null=False)
+    description_SEO = models.TextField(_('Description SEO'), max_length=5000, blank=True, null=False)
 
     class Meta:
         verbose_name_plural = _('Gallery')
