292c5f728b89ca59e11c434bb29ef5fd48b343ca
Kostiantyn Pidlisnyi <podlesny@outlook.com>
2019-03-13 17:50:34 +0200

    Update sw.js

--- src/static/custom/js/sw.js
+++ src/static/custom/js/sw.js
@@ -2,9 +2,16 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 const CACHE_NAME = 'workbox:js';
 
-const APP_JS = new RegExp('/static/custom/.*\.js');
-const APP_ANGULAR_JS = new RegExp('/static/custom/app/.*\.js');
-const VENDOR_JS = new RegExp('/static/vandor/.*\.js');
+const APP_JS = new RegExp('/static/custom/js/.*\.js');
+const APP_ANGULAR_JS = new RegExp('/static/custom/js/app/.*\.js');
+const VENDOR_JS = new RegExp('/static/vendor/js/.*\.js');
+
+workbox.routing.registerRoute(
+    '/',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'pages-home',
+    })
+  );
 
 workbox.routing.registerRoute(
     APP_JS,
