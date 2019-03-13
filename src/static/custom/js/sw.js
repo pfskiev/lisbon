4cd39056889fc24783510406fcc85ff017d9d0ac
Kostiantyn Pidlisnyi <podlesny@outlook.com>
2019-03-13 17:33:11 +0200

    Update sw.js

--- src/static/custom/js/sw.js
+++ src/static/custom/js/sw.js
@@ -1,13 +1,31 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-const CACHE_NAME = 'workbox:assets';
+const CACHE_NAME = 'workbox:js';
 
-// Cache all
-const REGEXP_ALL = /.*\.(?:js|css|png|jpg|jpeg|svg|gif)/;
+const APP_JS = new RegExp('/static/custom/.*\.js');
+const APP_ANGULAR_JS = new RegExp('/static/custom/app/.*\.js');
+const VENDOR_JS = new RegExp('/static/vandor/.*\.js');
+
 
 // [OK] Stale While Revalidate
 workbox.routing.registerRoute(
-  REGEXP_ALL,
+  APP_JS,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: `${CACHE_NAME}:stale-while-revalidate`,
+  }),
+);
+
+// [OK] Stale While Revalidate
+workbox.routing.registerRoute(
+  APP_ANGULAR_JS,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: `${CACHE_NAME}:stale-while-revalidate`,
+  }),
+);
+
+// [OK] Stale While Revalidate
+workbox.routing.registerRoute(
+  VENDOR_JS,
   workbox.strategies.staleWhileRevalidate({
     cacheName: `${CACHE_NAME}:stale-while-revalidate`,
   }),
